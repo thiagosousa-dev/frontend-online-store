c0e07c2042b7a9d57a66497c5080adce241b48e7
carolcabralm <carolcabralm@msn.com>
2022-02-09 17:25:15 -0300

    Req 2 - pag listagem

---
+++ src/pages/Home.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class Home extends React.Component {
+  render() {
+    return (
+      <div>
+        <input type="text" />
+        <p data-testid="home-initial-message">
+          Digite algum termo de pesquisa ou escolha uma categoria.
+        </p>
+      </div>
+    );
+  }
+}
+
+export default Home;
